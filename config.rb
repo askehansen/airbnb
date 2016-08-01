3474218388d9f7ae89bba77bbd09b2b0fc09adfe
Aske Hansen <aske@deeco.dk>
2016-08-01 18:41:11 +0200

    implement the show view

--- config.rb
+++ config.rb
@@ -31,6 +31,10 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+data.flats.each do |key, flat|
+  proxy "/flats/#{key}.html", '/flats/show.html', locals: { flat: flat }, ignore: true
+end
+
 ###
 # Helpers
 ###
@@ -44,11 +48,13 @@ activate :autoprefixer
 # end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  
+  def render_star(index, rating)
+    "<i class='fa fa-star'></i>"
+  end
+
+end
 
 set :css_dir, 'stylesheets'
 
